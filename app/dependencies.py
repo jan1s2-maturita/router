239377c00358247b79f4de0d05ec0c92cefa9735
Jan Str <janstr06@gmail.com>
2024-12-18 12:41:49 +0100

    update

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,8 +1,8 @@
 service_map = {
     "/auth": "http://auth:8000",
-    "/deploy": "http://deployer:8000",
-    "/list": "http://lister:8000",
-    "/delete": "http://deleter:8000",
+    "/deployer": "http://deployer:8000",
+    "/lister": "http://lister:8000",
+    "/deleter": "http://deleter:8000",
     "/admin": "http://admin:8000",
     "/flag-submit": "http://flag-submit:8000",
 }
